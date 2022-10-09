df4071e091ba7f3b1e925dd90e8dc99adaff742e
e1b20054 <e1b20054@oit.ac.jp>
2022-10-09 22:13:11 +0900

    じゃんけんの手を選んでCPUと対戦する処理の実装

--- src/main/java/oit/is/z0264/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z0264/kaizi/janken/controller/JankenController.java
@@ -15,6 +15,35 @@ public class JankenController {
     return "janken.html";
   }
 
+  @GetMapping("/janken.html")
+  public String janken2() {
+    return "janken.html";
+  }
+
+  @GetMapping("/hand_rock")
+  public String hand_rock(ModelMap model) {
+    model.addAttribute("YourHand", "グー");
+    model.addAttribute("EnemyHand", "グー");
+    model.addAttribute("Result", "Draw");
+    return "janken.html";
+  }
+
+  @GetMapping("/hand_scissors")
+  public String hand_scissors(ModelMap model) {
+    model.addAttribute("YourHand", "チョキ");
+    model.addAttribute("EnemyHand", "グー");
+    model.addAttribute("Result", "You Lose");
+    return "janken.html";
+  }
+
+  @GetMapping("/hand_paper")
+  public String hand_paper(ModelMap model) {
+    model.addAttribute("YourHand", "パー");
+    model.addAttribute("EnemyHand", "グー");
+    model.addAttribute("Result", "You Win!");
+    return "janken.html";
+  }
+
   @PostMapping("/janken")
   public String janken(@RequestParam String name, ModelMap model) {
     model.addAttribute("Name", name);
