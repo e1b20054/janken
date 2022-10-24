e95d10f5176d6b1f800788ffbc8874a13482e1b4
e1b20054 <e1b20054@oit.ac.jp>
2022-10-24 15:21:07 +0900

    試合を行い結果をデータベースに登録する処理の実装

--- src/main/java/oit/is/z0264/kaizi/janken/model/MatchMapper.java
+++ src/main/java/oit/is/z0264/kaizi/janken/model/MatchMapper.java
@@ -2,7 +2,7 @@ package oit.is.z0264.kaizi.janken.model;
 
 import java.util.ArrayList;
 
-//import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 //import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
@@ -12,5 +12,8 @@ public interface MatchMapper {
 
   @Select("SELECT * from Matches;")
   ArrayList<Match> selectAllMatch();
+
+  @Insert("INSERT INTO matches (user1, user2, user1Hand, user2Hand) VALUES (#{user1}, #{user2}, #{user1Hand}, #{user2Hand})")
+  void insertMatch(int user1, int user2, String user1Hand, String user2Hand);
 
 }
